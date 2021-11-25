036706b88f349a43a9297cc251a81b049cf4ca5d
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-25 15:05:28 +0100

    Añadido mock, testing de estilos de los elementos

--- cypress/integration/second-test.spec.js
+++ cypress/integration/second-test.spec.js
@@ -1,12 +1,16 @@
-describe("app", () => {
+describe("Página - Countries / Cities - Testeando funcionamiento y estilos", () => {
   beforeEach(() => {
-    cy.server(); // enable response stubbing
-    cy.route({
-      method: "GET", // Route all GET requests
-      url: "/countries", // that have a URL that matches '/countries'
-      response: [{ name: "Spain", capital: "Madrid" }], // and force the response to be this one
-    });
+    // Para Mockear la respuesta de la API sin tener que usarla
+    // tenemos que introducir la misma ruta que en la app y listo
+    // https://docs.cypress.io/api/commands/intercept#With-a-StaticResponse-object
+    const staticResponse = [{ name: "Spain", capital: "Madrid" }];
+    cy.intercept("/countries", staticResponse);
     cy.visit("/");
+  });
+
+  it("Check Country element take correct base styles", () => {
+    cy.get("li").invoke("css", "cursor").should("equal", "pointer");
+    cy.get("li").invoke("css", "width").should("equal", "300px");
   });
 
   it("should display city when clicking on country", () => {
@@ -14,28 +18,21 @@ describe("app", () => {
     cy.get("h1").contains("Madrid");
   });
 
-  it('Check City element take correct base styles', () => {
+  it("Check City element take correct base styles", () => {
     cy.contains("Spain").click();
-    
-    cy.get('h1').invoke('css', 'position')
-    .should('equal', 'fixed');
-    
-    cy.get('h1')
-    .invoke('css', 'color')
-    .should('equal', 'rgb(255, 255, 255)');
 
-    cy.get('h1')
-    .invoke('css', 'top')
-    .should('equal', '20px');
+    cy.get("h1").invoke("css", "position").should("equal", "fixed");
 
-    cy.get('h1')
-    .invoke('css', 'right')
-    .should('equal', '0px');
+    cy.get("h1").invoke("css", "color").should("equal", "rgb(255, 255, 255)");
 
-    cy.get('h1')
-    .invoke('css', 'text-align').should('equal', 'right');
+    cy.get("h1").invoke("css", "top").should("equal", "20px");
 
-    cy.get('h1')
-    .invoke('css', 'background-color').should('equal', 'rgba(133, 194, 10, 0.7)')
-  })
+    cy.get("h1").invoke("css", "right").should("equal", "0px");
+
+    cy.get("h1").invoke("css", "text-align").should("equal", "right");
+
+    cy.get("h1")
+      .invoke("css", "background-color")
+      .should("equal", "rgba(133, 194, 10, 0.7)");
+  });
 });
