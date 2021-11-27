028701103c2cdf4817d4e58f6c66e8afa59ffbf1
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-27 18:34:53 +0100

    Add token correct reference and add API repository reference url

--- src/app/@services/countries.service.ts
+++ src/app/@services/countries.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/internal/operators/map';
 import { ICountry } from '../@interfaces/country';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
-  private readonly url = 'http://localhost:3000/countries';
+  private readonly url = 'https://raw.githubusercontent.com/mugan86/angular-countries-app-with-testing/master/src/assets/db/countries.json';
   constructor(private httpClient: HttpClient) { }
   getCountries(): Observable<ICountry[]> {
-    return this.httpClient.get<ICountry[]>(this.url);
+    return this.httpClient.get<ICountry[]>(this.url).pipe(
+      map((result: any) => {
+        return result.countries
+      })
+    );
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://raw.githubusercontent.com/mugan86/angular-countries-app-with-testing/master/src/assets/db/countries.json'
 };
